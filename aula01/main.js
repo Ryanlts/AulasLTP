67ad95b727933e78595c106be65355e215960626
Ryanlts <rlsilva@aluno.riobrancofac.edu.br>
2023-08-24 22:36:32 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,16 @@
+//console.log eu uso para exibir uma mensagem
+console.log('Yolo Yolo');
+
+//alert mostra uma mensagem de alerta ao usuário
+alert('Hare Hare')
+
+//para declarar variáveis no js podemos usar "var", "let" ou "const"
+
+//sempre que usar "const" para declarar um valor de uma variável ele não pode ser alterado
+const pi = 3.14
+var nome = "Luan gameplays"
+
+
+alert(nome)
+
+alert(pi)
